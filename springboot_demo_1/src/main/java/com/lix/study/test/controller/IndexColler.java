0d11ae9c91a355ea9ebf7669f07044171a94bdf2
lix <sclx1220@163.com>
2018-11-05 14:10:50 +0800

    freemarker

---
+++ springboot_demo_1/src/main/java/com/lix/study/test/controller/IndexColler.java
@@ -0,0 +1,32 @@
+package com.lix.study.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lix on 2018/11/5/005.
+ */
+@Controller
+@RequestMapping("/test")
+public class IndexColler {
+
+    @RequestMapping("/index")
+    public String index(Map<String, Object> data){
+        data.put("name", "zhangsan");
+        data.put("age", 23);
+        data.put("sex", 1);
+        List<Map<String, Object>> friends = new ArrayList<>();
+        Map<String, Object> friend = new HashMap<>();
+        friend.put("name", "xiaoming");
+        friend.put("age", 22);
+        friends.add(friend);
+        data.put("friends", friends);
+        return "index";
+    }
+
+}
